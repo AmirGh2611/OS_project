0cb8324298a70258084310e6afbe93d276f36847
amirhossein <amirabbasghorbani354@gmail.com>
2025-12-11 13:40:22 +0330

    some improvements

--- Net.py
+++ Net.py
@@ -8,28 +8,38 @@ class ConvNet(nn.Module):
             nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(num_features=32),
             nn.ReLU(),
-            nn.AdaptiveMaxPool2d((28, 28))
+            nn.MaxPool2d(2)
         )
         self.layer2 = nn.Sequential(
             nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(num_features=64),
             nn.ReLU(),
-            nn.AdaptiveAvgPool2d((14, 14))
+            nn.AdaptiveMaxPool2d((14, 14))
         )
         self.layer3 = nn.Sequential(
             nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(num_features=128),
             nn.ReLU(),
-            nn.AdaptiveAvgPool2d((14, 14))
+            nn.AdaptiveAvgPool2d((7, 7))
         )
         self.layer4 = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(in_features=128 * 14 * 14, out_features=128),
-            nn.ReLU()
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(num_features=256),
+            nn.ReLU(),
+            nn.AdaptiveAvgPool2d((7, 7))
         )
         self.layer5 = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(in_features=256 * 7 * 7, out_features=256),
+            nn.ReLU(),
+            nn.Dropout(0.5)
+        )
+        self.layer6 = nn.Sequential(
+            nn.Linear(in_features=256, out_features=128),
+            nn.ReLU()
+        )
+        self.layer7 = nn.Sequential(
             nn.Linear(in_features=128, out_features=10),
-            nn.Softmax(dim=1)
         )
 
     def forward(self, x):
@@ -38,4 +48,6 @@ class ConvNet(nn.Module):
         out = self.layer3(out)
         out = self.layer4(out)
         out = self.layer5(out)
+        out = self.layer6(out)
+        out = self.layer7(out)
         return out
