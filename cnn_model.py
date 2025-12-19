7790141af9c0a7b017a6771e130ce0f9687f849c
amirhossein <amirabbasghorbani354@gmail.com>
2025-12-19 18:08:55 +0330

    improvement

--- cnn_model.py
+++ cnn_model.py
@@ -1,19 +1,21 @@
 from keras.models import Sequential
-from keras.layers import Dense, Conv2D, MaxPooling2D, Flatten, Dropout, BatchNormalization, Input
+from keras.layers import Dense, Conv2D, MaxPooling2D, Dropout, BatchNormalization, Input, \
+    GlobalAveragePooling2D
 
 model = Sequential([
     Input(shape=(28, 28, 1)),
     Conv2D(64, (3, 3), activation="relu", padding="same"),
     BatchNormalization(),
-    MaxPooling2D(pool_size=(2, 2)), # 14*14
+    Conv2D(64, (3, 3), activation="relu", padding="same"),
+    BatchNormalization(),
+    MaxPooling2D(pool_size=(2, 2)),
     Conv2D(128, (3, 3), activation="relu", padding="same"),
     BatchNormalization(),
-    MaxPooling2D(pool_size=(2, 2)), # 7*7
-    Flatten(),
-    Dense(128, activation="relu"),
+    GlobalAveragePooling2D(),
+    Dense(256, activation="relu", kernel_initializer='he_normal'),
     BatchNormalization(),
     Dropout(0.5),
-    Dense(64, activation="relu"),
+    Dense(128, activation="relu", kernel_initializer='he_normal'),
     BatchNormalization(),
     Dropout(0.5),
     Dense(10, activation="softmax"),
